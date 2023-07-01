785b07cf0777af39b8a7c2cb27c3f28e09edc30a
Chrisys-20 <chrisysdata@gmail.com>
2023-07-01 08:25:51 +0100

    ajout du js dans le code à travers les alcolade

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Employee from './components/Employee';
 
 function App() {
+
+  console.log('We are about to list the employees');
+  const showEmployees =true;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {console.log('inside the return')}
+      { 
+      showEmployees ?(
+        <>
+      <Employee/>
+      <Employee/>
+      <Employee/>
+      <Employee/>
+      </>
+      ):(<p>You cannot see the employees.</p>)
+      }
     </div>
   );
 }
---
+++ src/components/Employee.js
@@ -0,0 +1,9 @@
+
+
+function Employee(){
+    return (
+    <h3>Here is an Employee!</h3>
+    );
+}
+ 
+export default Employee;
