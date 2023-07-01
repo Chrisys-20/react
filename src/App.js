fb93a176fecad5de4864614142ee44b301d166d6
Chrisys-20 <chrisysdata@gmail.com>
2023-07-01 15:37:28 +0100

    getting ready for css

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import './App.css';
 import Employee from './components/Employee';
+import { useState } from 'react';
 
 function App() {
 
+  const [role, setRole]=useState('dev');
   const showEmployees =true;
+
   return (
     <div className="App">
       {console.log('inside the return')}
       { 
       showEmployees ?(
         <>
+        <input
+          type="text" onChange={(e)=>{
+            console.log(e.target.value); 
+            setRole(e.target.value);
+          }}
+        />
       <Employee name="Caleb" role="Inter"/>
-      <Employee />
+      <Employee name="Abby" role={role} />
       <Employee/>
       <Employee/>
       </>
